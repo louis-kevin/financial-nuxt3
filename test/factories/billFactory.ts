a92bdc944f759daf05ab2272f2de0ad21902d239
kevin.santos <kevin.santos@nubank.com.br>
2022-07-13 13:58:15 -0300

    created factory for bills, removed billState

---
+++ test/factories/billFactory.ts
@@ -0,0 +1,25 @@
+import { Bill, BillList } from "~/types";
+import { faker } from '@faker-js/faker/locale/pt_BR';
+
+export function createRandomBill(data: Partial<Bill> = {}): Bill {
+  return  {
+    id: faker.datatype.uuid(),
+    name: faker.name.jobTitle(),
+    amount: faker.datatype.number(),
+    createdAt: faker.date.past().toISOString(),
+    done: faker.datatype.boolean(),
+    ...data
+  };
+}
+
+export function createRandomBills(count: number, data: Partial<Bill> = {}): Bill[] {
+  return Array.from({ length: count }, () => createRandomBill(data));
+}
+
+export function createBillState(count: number, data: Partial<Bill> = {}): { list: BillList } {
+  const list = {} as BillList;
+  createRandomBills(count, data).forEach(bill => {
+    list[bill.id.toString().toLowerCase()] = bill;
+  })
+  return {list};
+}
